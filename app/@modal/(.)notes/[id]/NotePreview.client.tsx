2e86b0ac137803671d38d7977cdadae1cf3d449a
Andreas Sudorciuc <andreassydorchuk@gmail.com>
2026-07-19 18:57:13 +0200

    fix: modal

---
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+
+import { useParams, useRouter } from "next/navigation";
+
+import { fetchNoteById } from "../../../../lib/api";
+
+import Modal from "../../../../components/Modal/Modal";
+
+import css from "./NotePreview.module.css";
+
+export default function NotePreviewClient() {
+  const params = useParams();
+  const router = useRouter();
+
+  const id = params.id as string;
+
+  const handleClose = () => {
+    router.back();
+  };
+
+  const {
+    data: note,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["note", id],
+
+    queryFn: () => fetchNoteById(id),
+
+    refetchOnMount: false,
+  });
+
+  if (isLoading) {
+    return (
+      <Modal onClose={handleClose}>
+        <p>Loading, please wait...</p>
+      </Modal>
+    );
+  }
+
+  if (isError || !note) {
+    return (
+      <Modal onClose={handleClose}>
+        <p>Something went wrong.</p>
+      </Modal>
+    );
+  }
+
+  return (
+    <Modal onClose={handleClose}>
+      <div className={css.container}>
+        <div className={css.item}>
+          <div className={css.header}>
+            <h2>{note.title}</h2>
+          </div>
+
+          <p className={css.tag}>{note.tag}</p>
+
+          <p className={css.content}>{note.content}</p>
+
+          <p className={css.date}>{note.createdAt}</p>
+
+          <button type="button" className={css.backBtn} onClick={handleClose}>
+            ← Back
+          </button>
+        </div>
+      </div>
+    </Modal>
+  );
+}
